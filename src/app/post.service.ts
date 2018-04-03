d99f1993f110d2ab813738e0173114f33c959066
Anand T <19andt@gmail.com>
2018-04-03 18:16:45 +0530

    Updated post service

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -1,8 +1,46 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Injectable()
 export class PostService {
+  apiRoot: string = 'https://jsonplaceholder.typicode.com/posts';
+  results: Object[];
 
-  constructor() { }
+  constructor(private http: Http) { }
+
+  getList(){
+    let promise = new Promise((response, reject) => {
+      this.http.get(this.apiRoot)
+        .toPromise()
+        .then(
+          res => {
+            this.results = res.json();
+            response();
+          },
+          msg => {
+            reject(msg);
+          }
+        );
+    });
+    return promise;
+  }
+
+  getPostDetail(postId: number){
+    let promise = new Promise((response, reject) => {
+      let apiDetail = `${this.apiRoot}?id=${postId}`
+      this.http.get(apiDetail)
+        .toPromise()
+        .then(
+          res => {
+            this.results = res.json();
+            response();
+          },
+          msg => {
+            reject(msg);
+          }
+        );
+    });
+    return promise;
+  }
 
 }
