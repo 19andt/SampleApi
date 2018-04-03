31539028c2c6daa1692c393aed600a3a4e9bcb98
Anand T <19andt@gmail.com>
2018-04-03 18:15:41 +0530

    Updated user service

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,8 +1,46 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Injectable()
 export class UserService {
+  apiRoot: string = 'https://jsonplaceholder.typicode.com/users';
+  results: Object[];
 
-  constructor() { }
+  constructor(private http: Http) { }
+
+  getList(){
+    let promise = new Promise((response, reject) => {
+      this.http.get(this.apiRoot)
+        .toPromise()
+        .then(
+          res => {
+            this.results = res.json();
+            response();
+          },
+          msg => {
+            reject(msg);
+          }
+        );
+    });
+    return promise;
+  }
+
+  getDetail(userId: number){
+    let promise = new Promise((response, reject) => {
+      let apiDetail = `${this.apiRoot}?id=${userId}`
+      this.http.get(apiDetail)
+        .toPromise()
+        .then(
+          res => {
+            this.results = res.json();
+            response();
+          },
+          msg => {
+            reject(msg);
+          }
+        );
+    });
+    return promise;
+  }
 
 }
