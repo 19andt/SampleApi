685711bb1585821f141a76547e65f69e3af3e82a
Anand T <19andt@gmail.com>
2018-04-03 18:25:59 +0530

    Updated user detail component

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-user-detail',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-detail.component.css']
 })
 export class UserDetailComponent implements OnInit {
+  user: any;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute, private userApi: UserService) {
+    this.activatedRoute.paramMap
+      .subscribe(params => {
+        if(params.get('id') !== null) {
+          this.getUserDetail(+params.get('id'));
+        }
+      });
+  }
 
   ngOnInit() {
+  }
+
+  getUserDetail(id: number){
+    this.userApi.getDetail(id);
+    this.user = this.userApi.results[0];
   }
 
 }
