1395f3d8b03bc13bf683a93147b53aa40c0695d1
Anand T <19andt@gmail.com>
2018-04-03 18:16:57 +0530

    Updated comment service

--- src/app/comment.service.ts
+++ src/app/comment.service.ts
@@ -1,8 +1,64 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Injectable()
 export class CommentService {
+  apiRoot: string = 'https://jsonplaceholder.typicode.com/comments';
+  results: Object[];
 
-  constructor() { }
+  constructor(private http: Http) { }
+
+  getList(){
+    let promise = new Promise((response, reject) => {
+      this.http.get(this.apiRoot)
+        .toPromise()
+        .then(
+          res => {
+            this.results = res.json();
+            response();
+          },
+          msg => {
+            reject(msg);
+          }
+        );
+    });
+    return promise;
+  }
+
+  getCommentDetail(commentId: number){
+    let promise = new Promise((response, reject) => {
+      let apiDetail = `${this.apiRoot}?id=${commentId}`
+      this.http.get(apiDetail)
+        .toPromise()
+        .then(
+          res => {
+            this.results = res.json();
+            response();
+          },
+          msg => {
+            reject(msg);
+          }
+        );
+    });
+    return promise;
+  }
+
+  getCommentPost(postId: number){
+    let promise = new Promise((response, reject) => {
+      let apiDetail = `${this.apiRoot}?postId=${postId}`
+      this.http.get(apiDetail)
+        .toPromise()
+        .then(
+          res => {
+            this.results = res.json();
+            response();
+          },
+          msg => {
+            reject(msg);
+          }
+        );
+    });
+    return promise;
+  }
 
 }
